835725cd4b0c22de5a86d945695065f36779b4c7
Mark Vayngrib <mark@tradle.io>
2018-01-14 13:54:50 -0500

    feat: export validate

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
-module.exports = require('./merge')
+const { merge } = require('./merge')
+const validate = ({ models, model, lens }) => {
+  // defer to merge to validate
+  merge({ models, model, lens })
+}
+
+module.exports = {
+  merge,
+  validate
+}
--- test/index.js
+++ test/index.js
@@ -5,17 +5,19 @@ const models = require('@tradle/merge-models')()
   .add(require('@tradle/custom-models'))
   .get()
 
-const { merge } = require('../')
+const { merge, validate } = require('../')
 const valid = require('./fixtures/valid')
 const invalid = require('./fixtures/invalid')
 
 test('basic', t => {
   valid.forEach(lens => {
     t.doesNotThrow(() => merge({ models, lens }))
+    t.doesNotThrow(() => validate({ models, lens }))
   })
 
   invalid.forEach(({ lens, error }) => {
     t.throws(() => merge({ models, lens }), error)
+    t.throws(() => validate({ models, lens }), error)
   })
 
   t.end()
